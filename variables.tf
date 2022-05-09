e6a56a38158080d1fc46dc73d541c75936737530
polakavya <102403334+polakavya@users.noreply.github.com>
2022-05-09 10:50:32 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+       description = "The AWS region to create things in."
+       default     = "us-east-1"
+}
+
+variable "key_name" {
+    description = " SSH keys to connect to ec2 instance"
+    default     =  "SL"
+}
+
+variable "instance_type" {
+    description = "instance type for ec2"
+    default     =  "t2.micro"
+}
+
+variable "security_group" {
+    description = "Name of security group"
+    default    = "jenkins_security_group1"
+}
+variable "tag_name" {
+    description = "Tag Name of for Ec2 instance"
+    default     = "my-ec2-instance"
+}
+variable "ami_id" {
+    description = "AMI for Ubuntu Ec2 instance"
+    default     = "ami-0c4f7023847b90238"
+}
